4f38cb4e9da8ec475c8e5cc8e14398ae80d588cd
MikeShalin <mixailshalin@yandex.ru>
2017-02-04 21:13:56 +0400

    Delete core.php

--- core.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $db = mysql_connect("localhost", "mixailp7_chat", "6]*xyFDd");
-    mysql_select_db("mixailp7_chat", $db) or die("Нет соединения с БД! " . mysql_error());
-
-    ini_set( "date.timezone", "Europe/Samara" );
-    mysql_set_charset('utf8'); // добавил кодировку БД
-
-?>
